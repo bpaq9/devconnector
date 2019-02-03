38714240aa94cbc18e612087e17a2423617052bb
Ben Paquin <bpaquin9@gmail.com>
2019-02-02 19:35:51 -0500

    creates key for JWT

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://ben:pickles1@ds163354.mlab.com:63354/devconnector"
+  mongoURI: "mongodb://ben:pickles1@ds163354.mlab.com:63354/devconnector",
+  secretOrKey: "secret"
 };
