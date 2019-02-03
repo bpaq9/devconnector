fe847f8c8fb89a9b51ff97bc7cced5138d9f8265
Ben Paquin <bpaquin9@gmail.com>
2019-02-02 19:58:15 -0500

    creates JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
 
 //Load user model
 const User = require("../../models/User");
@@ -24,7 +26,6 @@ router.post("/register", (req, res) => {
         r: "pg",
         d: "mm"
       });
-
       const newUser = new User({
         name: req.body.name,
         email: req.body.name,
@@ -62,7 +63,22 @@ router.post("/login", (req, res) => {
     // Check Password
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        res.json({ msg: "Success" });
+        //User Matched
+
+        const payload = { id: user.id, name: user.name, avatar: user.avatar }; //Create JWT Payload
+
+        //Sign Token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
       } else {
         return res.status(400).json({ password: "Password incorrect" });
       }
