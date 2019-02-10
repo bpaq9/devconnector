f94773a8ed662024be8e3df91ed3af7304c300c4
Ben Paquin <bpaquin9@gmail.com>
2019-02-10 18:55:19 -0500

    fixes typos

--- validation/education.js
+++ validation/education.js
@@ -1,7 +1,7 @@
 const Validator = require("validator");
 const isEmpty = require("./is-empty");
 
-module.exports = function validateEducationinput(data) {
+module.exports = function validateEducationInput(data) {
   let errors = {};
 
   data.school = !isEmpty(data.school) ? data.school : "";
@@ -18,7 +18,7 @@ module.exports = function validateEducationinput(data) {
   }
 
   if (Validator.isEmpty(data.fieldofstudy)) {
-    errors.fieldofstudy = "Field of study date field is required";
+    errors.fieldofstudy = "Field of study field is required";
   }
 
   if (Validator.isEmpty(data.from)) {
