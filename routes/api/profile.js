88bd330a064d12f982b5cf01557d1dc116e812e8
Ben Paquin <bpaquin9@gmail.com>
2019-02-10 18:47:41 -0500

    Update profile.js

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -276,4 +276,19 @@ router.delete(
   }
 );
 
+// @route Delete api/profile/
+//@description Delete user and profile
+//@access Private
+router.delete(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id }).then(() => {
+      User.findOneAndRemove({ _id: req.user.id }).then(() =>
+        res.json({ success: true })
+      );
+    });
+  }
+);
+
 module.exports = router;
