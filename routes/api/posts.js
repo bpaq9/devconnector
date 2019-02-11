99794ee61a9e25f53536128699ce2fc5b96de896
Ben Paquin <bpaquin9@gmail.com>
2019-02-10 19:52:10 -0500

    adds validation for new posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Post model
+const Post = require("../../models/Post");
+
+// Validation
+const validatePostInput = require("../../validation/post");
 
 // @route GET api/posts/test
 //@description Tests post route
 //@access Public
 router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
+
+// @route Post api/posts
+//@description Create post
+//@access Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    if (!Valid) {
+      // If any errors, send 400 with errors objec
+      return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.name,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
 
 module.exports = router;
